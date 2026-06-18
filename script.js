04b40022feb46d23811866b0d34803f607a7ef95
Dawid108 <gustowskidawid108@gmail.com>
2026-06-18 18:31:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,10 +37,28 @@ window.onload = function() {
     }
 };
 
-let dawid108 = parseInt(localStorage.getItem("dawid108")) || 0;
+let czas108 = localStorage.getItem("czas108") || 0;
 
-// Dodawanie 1 punktu co 5 sekund
 setInterval(() => {
-    dawid108++;
-    localStorage.setItem("dawid108", dawid108);
-}, 5000);
+    czas108 += 10;
+    localStorage.setItem("czas108", czas108);
+}, 10000);
+
+let id108 = localStorage.getItem("id108");
+
+if (
+    !id108 ||
+    isNaN(id108)
+) {
+    id108 =
+    Math.floor(Math.random() * 9999) + 1;
+
+    localStorage.setItem(
+        "id108",
+        id108
+    );
+
+    console.log("Nowy numer:", id108);
+} else {
+    console.log("Numer:", id108);
+}
